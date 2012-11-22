71abbe092c16e12605808a77bb1c85e478f3babf
Eriq Augustine <eriq.augustine@gmail.com>
2012-11-22 09:52:51 -0800

    Done with parsing out comments. Will probably encapsulate all data in a class soon.

--- code/parser.py
+++ code/parser.py
@@ -1,5 +1,12 @@
 import os
+import re
 import sys
+
+# Use a more complicated marker for citations to reduce chance of collisions.
+CITATION_MARKER = '[<?>]'
+# This is used when there are multiple citation in a context.
+# This will mark the citation you are currently looking at.
+MARKED_CITATION_MARKER = '[<X>]'
 
 def parseMeta(fileName):
    fileObj = open(fileName, 'r')
@@ -19,8 +26,7 @@ def parseMeta(fileName):
       abstract += " " + line
       line = fileObj.readline().strip()
 
-   #rtn['abstract'] = abstract
-   rtn['abstract'] = 'ABSTRACT'
+   rtn['abstract'] = abstract
 
    return rtn
 
@@ -33,8 +39,7 @@ def parseDir(dirName):
 
    if os.path.exists(dirName + '/paper.txt'):
       fileObj = open(dirName + '/paper.txt')
-      entry['fullText'] = 'TEXT'
-      #entry['fullText'] = fileObj.read().strip()
+      entry['fullText'] = fileObj.read().strip()
    else:
       entry['fullText'] = none
 
@@ -44,6 +49,71 @@ def parseDir(dirName):
       entry['pdfPath'] = none
 
    return entry
+
+# This is the all purpose citation removeing method.
+# |entry| is assumed to be a entry that was created with parseDir().
+# It MUST HAVE 'fullText'.
+# 'noNumbersText', 'noCitationsText', 'citations', and 'citationKey' fields will be added to |entry|.
+# 'citations' will be a randomized list of contexts for the citations.
+# 'citationKey' will be an array of citations in the order that they appear in the text.
+# Both 'noNumbersText' and 'noCitationsText' will not contain anything in or after after the references section.
+# NOTE: This will probably not be effective if it is not one paragraph per line.
+def parseCitations(entry):
+   noNumbersText = ''
+   noCitationsText = ''
+   citations = []
+   citationKey = []
+   # For when we see the references
+   noMoreCitations = False
+
+   # First, make a global replacement for multiple cites eg. '[1, 2]'
+   parsedText = entry['fullText']
+   groupedCitations = re.findall('\[\s*\d+\s*(?:,\s*\d+\s*)+\]', parsedText)
+   for cite in groupedCitations:
+      numbers = re.findall('\d+', cite)
+      replacement = ''
+      for number in numbers:
+         replacement += '[{0}]'.format(number)
+      parsedText = parsedText.replace(cite, replacement)
+
+   # Go line by line.
+   for line in parsedText.splitlines():
+      rawLine = line.strip()
+
+      if re.search('REFERENCES', rawLine):
+         noMoreCitations = True
+
+      if not noMoreCitations and re.search('\[\s*\d+\s*\]', rawLine):
+         cites = re.findall('\[\s*\d+\s*\]', rawLine)
+
+         noCitationsLine = re.sub('\[\s*\d+\s*\]', '', rawLine)
+         noNumbersLine = re.sub('\[\s*\d+\s*\]', CITATION_MARKER, rawLine)
+
+         noCitationsText += noCitationsLine + "\n"
+         noNumbersText += noNumbersLine + "\n"
+
+         for cite in cites:
+            citeNum = int(re.search('(\d+)', cite).group(1))
+            citationKey.append(citeNum)
+
+            markedLine = rawLine.replace(cite, MARKED_CITATION_MARKER)
+            markedLine = re.sub('\[\s*\d+\s*\]', CITATION_MARKER, markedLine)
+
+            #TODO(eriq): Sentence.
+            citations.append({'paragraph': rawLine,
+                              'noCitesParagraph': noCitationsLine,
+                              'noNumbersParagraph': noNumbersLine,
+                              'markedParagraph': markedLine,
+                              'numberOfCitesInParagraph': len(cites)
+                             })
+      else:
+         noCitationsText += rawLine + "\n"
+         noNumbersText += rawLine + "\n"
+
+   entry['noCitationsText'] = noCitationsText
+   entry['noNumbersText'] = noNumbersText
+   entry['citationKey'] = citationKey
+   entry['citations'] = citations
 
 
 # Parse an entire directory structure.
@@ -57,8 +127,11 @@ def parseFullDataset(baseDir):
    for fileName in os.listdir(baseDir + "/references"):
       rtn['references'][int(fileName)] = parseDir(baseDir + "/references/" + fileName)
 
+   parseCitations(rtn['root'])
+
    return rtn
 
 if __name__ == "__main__":
    #print parseDir('data/dynamo')
-   print parseFullDataset('data/dynamo')
+   #print parseFullDataset('data/dynamo')
+   parseFullDataset('data/dynamo')
