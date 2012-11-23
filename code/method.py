c60b78cfcb554f5761a0fbfaef334b781bca21d7
Eriq Augustine <eriq.augustine@gmail.com>
2012-11-23 14:59:27 -0800

    Added stopwords

--- code/method.py
+++ code/method.py
@@ -6,6 +6,9 @@ import util
 # The hope is that every algorithm extends Method in the same pattern
 # and then they can be switched out.
 # Utility methods (perhaps a special split string or stopword removal) should probably go in here.
+# WARNING: A Method may maintain state data about the citations it has done.
+#  This is very important in the cases that there are multiple cites in a single context (sentence, paragraph, etc).
+#  Therefore, you should only use a method instance on one citation cycle (itteration through the paper).
 class Method:
    # Return None if you can't make a guess.
    def guess(cite, paper):
@@ -22,16 +25,30 @@ class ProperNounMethod(Method):
          authorNouns = re.findall('\w{2,}', ' '.join(reference.authors))
          self.properNouns[key] = set([authorNoun.upper() for authorNoun in authorNouns])
 
-         titleNouns = re.findall('\w{2,}', reference.title)
-         self.properNouns[key] |= set([noun.upper() for noun in titleNouns])
+         self.properNouns[key] |= util.removeTitleStopwords(util.getCapitalWords(reference.title))
+
+         #self.properNouns[key] |= util.getCapitalWords(reference.abstract)
 
       self.properNouns = util.uniqueSets(self.properNouns)
+
+      #TEST
+      for (ref, nouns) in self.properNouns.items():
+         print "{0} -- {1}".format(ref, nouns)
+
+      # There will be no more than one citation per reference per context (probably).
+      self.contextHistory = {}
 
    def guess(self, cite, paper):
       words = util.getCapitalWords(cite.sentenceContext.noCitations)
 
+      # Get the citation history for this context
+      if not self.contextHistory.has_key(cite.sentenceContext.noCitations):
+         self.contextHistory[cite.sentenceContext.noCitations] = set()
+      history = self.contextHistory[cite.sentenceContext.noCitations]
+
       for (referenceKey, nouns) in self.properNouns.items():
-         if len(nouns & words) > 0:
+         if not referenceKey in history and len(nouns & words) > 0:
+            history.add(referenceKey)
             return int(referenceKey)
 
       return None
